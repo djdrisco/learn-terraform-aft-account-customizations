b956d38d06e082d3f17baee137a6f106a052668e
driscolld <david.driscoll@cfpb.gov>
2022-01-05 12:41:46 -0500

    add 1st ekstest account customization creates s3 bucket

---
+++ ekstest/terraform/s3.tf
@@ -0,0 +1,6 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "ekstest_bucket" {
+  bucket = "aft-ekstest-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
