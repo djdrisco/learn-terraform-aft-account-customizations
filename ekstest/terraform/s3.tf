95c591bea5cb2681b72b585e6703bb956c16f215
driscolld <david.driscoll@cfpb.gov>
2022-01-06 10:26:31 -0500

    more eks testing

--- ekstest/terraform/s3.tf
+++ ekstest/terraform/s3.tf
@@ -1,4 +1,3 @@
-data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "ekstest_bucket" {
   bucket = "aft-ekstest-${data.aws_caller_identity.current.account_id}"
--- ekstest/terraform/versions.tf
+++ ekstest/terraform/versions.tf
@@ -1,31 +1,14 @@
 terraform {
+  required_version = ">= 0.13.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = ">= 3.64"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.1.0"
-    }
-
     null = {
       source  = "hashicorp/null"
-      version = "3.1.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.1"
+      version = ">= 3.0"
     }
   }
-
-  required_version = ">= 0.14"
 }
-
